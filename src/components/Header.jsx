687d0938296625476e13289a0938e24ffe7a705a
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2023-12-30 21:53:50 +0600

    adding a todo working properly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,27 @@
 import notes from "../assets/images/notes.png";
 import doubleTick from "../assets/images/double-tick.png";
 import plus from "../assets/images/plus.png";
+import { addTodo } from "../redux/todos/actions";
+import { useDispatch } from "react-redux";
 
 export default function Header() {
+  const dispatch = useDispatch();
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    const todo = e.target.todo.value;
+    dispatch(addTodo(todo));
+  };
+
   return (
     <div>
-      <form className="flex items-center bg-gray-100 px-4 py-4 rounded-md">
+      <form
+        onSubmit={handleAddTodo}
+        className="flex items-center bg-gray-100 px-4 py-4 rounded-md"
+      >
         <img src={notes} className="w-6 h-6" alt="Add todo" />
         <input
+          name="todo"
           type="text"
           placeholder="Type your todo"
           className="w-full text-lg px-4 py-1 border-none outline-none bg-gray-100 text-gray-500"
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,11 +3,12 @@ import { useSelector } from "react-redux";
 
 export default function Todos() {
   const todos = useSelector((state) => state.todos);
-  console.log(todos);
 
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      <Todo />
+      {todos.map((todo, key) => (
+        <Todo key={key} todo={todo} />
+      ))}
     </div>
   );
 }
